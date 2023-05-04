ec4c1be16e5072a4e6100cf25ad20224cdeafcf6
RONI COSTA <profronicosta@gmail.com>
2023-05-03 21:35:01 -0300

    tradução do texto

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-nome = input("Digite seu nome")
-print("Boa Noite")
-print("Seja Bem Vindo " + nome)
-print("Obrigado por visitar nosso site")
+nome = input("Type your name")
+print("Good Night")
+print("Welcome " + nome)
+print("Thanks!")
